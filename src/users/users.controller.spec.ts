98e10d1d8696a19fdd6d20a4c08d87f950be21d7
Aleksey Kim <alex.romanovich.ak@gmail.com>
2024-02-23 15:13:07 +0900

    test: returning 1 user with the given id(findOne), updating session object and returning user

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -28,7 +28,9 @@ describe('UsersController', () => {
 
 		fakeAuthService = {
 			// signup: () => {},
-			// signin: () => {},
+			signin: (email: string, password: string) => {
+				return Promise.resolve({ id: 1, email, password } as User)
+			},
 		}
 
 		const module: TestingModule = await Test.createTestingModule({
@@ -55,13 +57,34 @@ describe('UsersController', () => {
 	})
 	it('findAllUsers returns a list of users with the given email', async () => {
 		const users = await controller.findAllUsers('asdf@asdf.com')
-    	//*** Comment: to check the validity of the test, change the number in toEqual(1) and get an error when testing
+		//*** Comment: to check the validity of the test, change the number in toEqual(1) and get an error when testing
 		expect(users.length).toEqual(1)
 		expect(users[0].email).toEqual('asdf@asdf.com')
 	})
 
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+
 	it('findUser throws an error if user with given id is not found', async () => {
 		fakeUsersService.findOne = () => null
 		await expect(controller.findUser('1')).rejects.toThrow(NotFoundException)
+	})
+
+	it(' sign in, updates session object and returns user', async () => {
+    //*** Comment: session userId is -10 because we need invalid value to make sure that it work the way we want it to work  if we got -10, it means we didn't update the session correctly, if we updated it correctly, we should get 1
+		const session = {userId: -10}
+		const user = await controller.signin(
+			{ email: 'asdf@asdf.com', password: 'asdf' },
+			session
+		)
+
+    //*** Comment: user.id is 1 because initially 'signin' method was initialized with that value above***//
+    expect(user.id).toEqual(1)
+
+    expect(session.userId).toEqual(1)
 	})
 })
